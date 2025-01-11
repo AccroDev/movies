b6f3e031d0f331910988a3473a9396db27940cb3
AccroDev <josephemrys3@gmail.com>
2025-01-11 06:22:30 -0800

    Add Shops Modal

---
+++ src/components/Modals/AddShops.vue
@@ -0,0 +1,66 @@
+<script setup>
+    const showTown = ref(false);
+    import { ref } from 'vue';
+
+    const classTownsElements = ref("hidden"); 
+    function ToggleTown() {
+        showTown.value = !showTown.value;
+        if (showTown.value) {
+            classTownsElements.value = "scale-y-0"
+            setTimeout(() => { 
+                classTownsElements.value = "scale-y-100" 
+            }, 3);
+        } else {
+            classTownsElements.value = "scale-y-0"
+            setTimeout(() => {
+                classTownsElements.value = "hidden"
+            }, 300);
+        }
+    } 
+    const emit = defineEmits(["closeAddModal"])
+</script>
+
+<template>
+    <div class="fixed top-0 left-0 bottom-0 right-0 bg-black/80 w-full h-full z-20 flex justify-center items-center " @click="emit('closeAddModal')" >
+        <div @click.stop class="bg-white rounded-md w-[95%] sm:w-4/5 max-w-2xl h-[90%] overflow-hidden flex justify-center items-center p-2 sm:p-4 addShopWrapper relative" >
+            <div class="w-full sm:w-4/5 " > 
+                    <h2 class="text-2xl font-bold mb-6 text-center text-back">Créer une Boutique</h2>
+                    <div class="mb-4">
+                        <label for="name" class="block text-sm font-medium text-black">Nom De la Boutique</label>
+                        <input type="text" id="name" required class="mt-1 block w-full px-3 py-2 border-b border-black focus:outline-none sm:text-sm bg-transparent text-black">
+                    </div> 
+                    <div class="mb-4">
+                        <label for="name" class="block text-sm font-medium text-black">Miniature</label>
+                        <input type="file" id="name" required class="mt-1 block w-full px-3 py-2 border-b border-black focus:outline-none sm:text-sm bg-transparent text-black">
+                    </div> 
+                    <div class="mb-4" >
+                        <label for="name" class="block text-sm font-medium text-black">Ville</label>
+                        <div class="relative w-full h-10 border-b border-black"  @click="ToggleTown()">
+                            <div class="flex flex-1 justify-between items-center mx-1 cursor-pointer ">
+                                <h5 class="font-semibold text-lg  uppercase filterTitle">Bujumbura</h5>
+                                <span  class="fas fa-arrow-down font-semibold"></span>
+                            </div> 
+                            <ul class="absolute top-10 left-0  right-0 origin-top scale-y-0 px-2 py-1 bg-[#feebd6] rounded-md duration-200 transition-all z-10" :class="classTownsElements" >
+                                <li class="rounded-sm hover:font-semibold py-1 text-xs px-1 cursor-pointer hover:bg-[#e9d5c1]" >Bujumbura</li>
+                                <li class="rounded-sm hover:font-semibold py-1 text-xs px-1 cursor-pointer hover:bg-[#e9d5c1]" >Goma</li>
+                                <li class="rounded-sm hover:font-semibold py-1 text-xs px-1 cursor-pointer hover:bg-[#e9d5c1]" >Bukavu</li>
+                                <li class="rounded-sm hover:font-semibold py-1 text-xs px-1 cursor-pointer hover:bg-[#e9d5c1]" >Minova</li>
+                            </ul>
+                        </div>
+                    </div>   
+                    <div class="mb-6">
+                        <label for="telephone" class="block text-sm font-medium text-black">Télephone</label>
+                        <input type="tel" id="telephone" required class="mt-1 block w-full px-3 py-2 border-b border-black focus:outline-none  sm:text-sm bg-transparent text-black">
+                    </div> 
+                    <div class="mb-6">
+                        <label for="adress" class="block text-sm font-medium text-black">Adress</label>
+                        <input type="text" id="adress" required class="mt-1 block w-full px-3 py-2 border-b border-black focus:outline-none  sm:text-sm bg-transparent text-black">
+                    </div> 
+                    <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none ">Enregistrer</button>
+            </div>
+            <button @click="emit('closeAddModal')" class="absolute top-2 right-1 rounded-sm px-3 py-1 text-xs bg-red-950 opacity-80 text-white font-medium hover:opacity-60 duration-300" >Fermer</button>
+        </div>
+    </div>
+</template>
+ 
+ 
--- src/components/ProfilComponents/MyShops.vue
+++ src/components/ProfilComponents/MyShops.vue
@@ -1,18 +1,59 @@
 <script setup>
     import ShopCard from '../Cards/ShopCard.vue';
+    import AddShops from '../Modals/AddShops.vue';
+    import { ref } from 'vue';
+    const showAssShop = ref(false)
+    const events = defineEmits(['close']) 
 </script>
 
 <template>
-    <section class="max-w-3xl mx-auto p-2 md:p-0">
+    <section class="max-w-3xl mx-auto p-2 md:p-0" >
         <h3 class="font-semibold text-lg border-b-4 border-[#fdae5c] max-w-max " >Mes Boutiques De Transfert</h3>
         <div class="containerShops flex flex-wrap" >
             <ShopCard src="/src/assets/img/user.jpg" />
             <ShopCard src="/src/assets/img/edition.webp" />
         </div>
-        <button class="text-white bg-blue-500 rounded-md px-2 py-1 my-2 hover:opacity-85 transition-opacity duration-300 w-full max-w-72 min-h-12 text-xl " > <i class="fas fa-plus" ></i> Créer Une Boutique </button>
+        <button class="text-white bg-blue-500 rounded-md px-2 py-1 my-2 hover:opacity-85 transition-opacity duration-300 w-full max-w-72 min-h-12 text-xl " @click="showAssShop = !showAssShop" > <i class="fas fa-plus" ></i> Créer Une Boutique </button>
+        <transition name="slide" >
+            <AddShops :key="'one'" v-if="showAssShop" @closeAddModal="showAssShop = !showAssShop" />
+        </transition>
     </section>
+    
 </template>
 
 <style>
-   
+    .slide-enter-active, .slide-leave-active {
+      transition: 0.3s ease;
+    }
+    .slide-enter-active .addShopWrapper, .slide-leave-active .addShopWrapper{
+      transition: 0.3s ease;
+    }
+   .slide-enter-from { 
+      opacity: 0; 
+    }
+
+    .slide-enter-to { 
+        opacity: 1; 
+    }
+   .slide-enter-from .addShopWrapper{ 
+      transform: translateY(30px);
+    }
+
+    .slide-enter-to .addShopWrapper{ 
+        transform: translateY(0px);
+    }
+   .slide-leave-from .addShopWrapper{ 
+      transform: translateY(0px);
+    }
+
+    .slide-leave-to .addShopWrapper{ 
+        transform: translateY(30px);
+    }
+   .slide-leave-from { 
+      opacity: 1;
+    }
+
+    .slide-leave-to { 
+        opacity: 0;
+    } 
 </style>
